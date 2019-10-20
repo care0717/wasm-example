bba18cbdc9179dce39af7b5fe794325c58a0d2f7
asai <dev.care0717@gmail.com>
2019-10-20 15:05:06 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import(
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"syscall/js"
+)
+
+
+var stack string
+
+func inputNum(this js.Value, i []js.Value) interface{} {
+	// 引数を文字列に変換して連結する
+	stack += i[0].String()
+	setResult(stack)
+	return nil
+}
+
+func doClear(this js.Value, i []js.Value) interface{} {
+	clear()
+	setResult(stack)
+	return nil
+}
+
+func clear() {
+	stack = ""
+}
+
+func setResult(res string) {
+	js.Global().Get("document").Call("getElementById", "result").Set("textContent", res)
+}
+
+
+func isOpe(b byte) bool {
+	switch b {
+	case '/','+','-','*':
+		return true
+	default:
+		return false
+	}
+}
+
+func inputOpe(this js.Value, i []js.Value) interface{} {
+	if len(stack) == 0 {
+		return nil
+	}
+	if isOpe(stack[len(stack)-1]) {
+		stack = stack[:len(stack)-1]
+	}
+	stack += i[0].String()
+	setResult(stack)
+	return nil
+}
+
+func calc(stack string) (int, error) {
+	ope := regexp.MustCompile(`/|\*|\+|-`)
+	num := regexp.MustCompile(`\d+`)
+	nums := ope.Split(stack, -1)
+	numbers := make([]int, len(nums))
+	for i, v := range(nums) {
+		n, _ := strconv.Atoi(v)
+		numbers[i] = n
+	}
+	opes := num.ReplaceAllString(stack, "")
+	res, err := cal(numbers, opes)
+	if err != nil {
+		return 0, err
+	}
+
+	return res, nil
+}
+
+func cal(numbers []int, opes string) (int, error) {
+	if len(numbers) == 1 {
+		return numbers[0], nil
+	}
+	tmp, err := c(opes[0], numbers[0], numbers[1])
+	if err != nil {
+		return 0, err
+	}
+	numbers[1] = tmp
+	numbers = numbers[1:]
+	opes = opes[1:]
+	return cal(numbers, opes)
+}
+func c(ope byte, v1 int, v2 int) (int, error) {
+	switch ope {
+	case '+':
+		return v1 + v2, nil
+	case '-':
+		return v1-v2, nil
+	case '/':
+		return v1/v2, nil
+	case '*':
+		return v1*v2, nil
+	default:
+		return 0, errors.New("undefined operator")
+	}
+}
+
+func doCalc(this js.Value, i []js.Value) interface{} {
+	if len(stack) == 0 || isOpe(stack[len(stack)-1]) {
+		return nil
+	}
+
+	res, err := calc(stack)
+	if err != nil {
+		clear()
+		setResult(err.Error())
+	}
+	stack = strconv.Itoa(res)
+	setResult(stack)
+	return nil
+}
+
+func print(this js.Value, i []js.Value) interface{} {
+	fmt.Println(i)
+	return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("print", js.FuncOf(print))
+	js.Global().Set("inputNum", js.FuncOf(inputNum))
+	js.Global().Set("inputOpe", js.FuncOf(inputOpe))
+	js.Global().Set("doCalc", js.FuncOf(doCalc))
+	js.Global().Set("doClear", js.FuncOf(doClear))
+}
+
+func main(){
+	c := make(chan struct{}, 0)
+	registerCallbacks()
+	<-c
+}
